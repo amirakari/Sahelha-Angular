d2228323ba27ae7b821185255360e2c18805745c
Akari Amir <amir.akari@esprit.tn>
2021-06-10 22:14:14 +0200

    10/06/2021

---
+++ src/app/utilisateur/listeusers/listeusers.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserService} from '../user.service';
+import {Utilisateur} from '../../Model/Utilisateur';
+import {MenuItem} from 'primeng/api';
+import {Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
+
+@Component({
+  selector: 'app-listeusers',
+  templateUrl: './listeusers.component.html',
+  styleUrls: ['./listeusers.component.css']
+})
+export class ListeusersComponent implements OnInit {
+
+  constructor(private userService: UserService,
+              private router: Router, ) { }
+  @Input() user1: Utilisateur[];
+  http: string;
+  totalRecords: number;
+  page = 1;
+  items: MenuItem[];
+  ngOnInit(): void {
+    this.http = environment.http;
+    this.userService.getUsers().subscribe(
+      (user) => {
+        this.user1 = user;
+        this.totalRecords = user.length;
+         });
+  }
+  bloquer(id: number){
+    this.userService.supprimertilisateur(id).subscribe(
+      (user) => {
+        const link = ['listeusers'];
+        this.router.navigate(link);
+      });
+  }
+}
