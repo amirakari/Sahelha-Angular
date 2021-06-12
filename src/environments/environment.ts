4135c01070f9f36e66b5a90908995c56acbe2513
Akari Amir <amir.akari@esprit.tn>
2021-06-12 15:45:02 +0200

    deployment 2.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
  // googleMapsKey: 'AIzaSyDyKbqeWTkZVdS99nz6SO3ypKlY_sJ2HRU',
-  http: 'http://localhost:3000',
-  capatcha: '6LfcvH0aAAAAAJHHlhuW547XmFI6Mz4oga1Z_k3h'
+  http: 'http://zerogaspii.com:3000',
+  capatcha: '6Lc_fisbAAAAAJAbIrixt09cUMvEUyTpo-IwPhqH'
 };
 
 /*
