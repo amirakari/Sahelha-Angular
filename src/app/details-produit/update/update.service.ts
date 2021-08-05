db0b9f92ccfc68d4b500a31705c80bb1f7dce1a4
Akari Amir <amir.akari@esprit.tn>
2021-08-05 12:37:01 +0200

    05/08/2021

--- src/app/details-produit/update/update.service.ts
+++ src/app/details-produit/update/update.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Produit} from '../../Model/Produit';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
-  link = 'http://localhost:3000/produit';
+  link = 'http://165.22.85.49:3000' + '/produit';
   constructor(private http: HttpClient) { }
   addUtilisateur(utilisateur: Produit, id): Observable<any>{
     return  this.http.patch(this.link + `/${id}` , utilisateur);
